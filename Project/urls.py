2a46f10b649a01fc5e41f49d95ddf717d4015788
Zakarya <ZakaryaCountu>
2020-10-24 12:43:39 +0100

    add new app[Home]

--- Project/urls.py
+++ Project/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Home/', include('Home.urls',namespace='HomePage')),
 ]
+
+#for static files::
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#media files ::
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
